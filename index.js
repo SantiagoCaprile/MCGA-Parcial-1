fe91556bc40bc896332518fb2e98d2edc63ad85b
Santiago Caprile <santiagocaprile7@gmail.com>
2022-09-29 18:39:07 -0300

    Primer commit. Server funcionando con conexion a la bd

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config( { path: 'creds.env' } );
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`)
+})
